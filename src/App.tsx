15e4e1e0189cc6eaece8f0700013c297f0a7e708
EmmanuelCahu <134721876+EmmanuelCahu@users.noreply.github.com>
2025-06-02 10:18:55 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import { Routes, Route } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Ativos from './pages/Ativos'
 import Mercado from './pages/Mercado'
 import Recomendacoes from './pages/Recomendacoes'
 import Educacao from './pages/Educacao'
-import Navbar from './components/Navbar'
 
-function App() {
+const App = () => {
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-900">
+    <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/ativos" element={<Ativos />} />
-        <Route path="/mercado" element={<Mercado />} />
-        <Route path="/recomendacoes" element={<Recomendacoes />} />
-        <Route path="/educacao" element={<Educacao />} />
-      </Routes>
-    </div>
+      <main className="container mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ativos" element={<Ativos />} />
+          <Route path="/mercado" element={<Mercado />} />
+          <Route path="/recomendacoes" element={<Recomendacoes />} />
+          <Route path="/educacao" element={<Educacao />} />
+        </Routes>
+      </main>
+    </Router>
   )
 }
 

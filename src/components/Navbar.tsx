f71f9a6b68f3d664667c2fe7924ad90968055940
EmmanuelCahu <134721876+EmmanuelCahu@users.noreply.github.com>
2025-06-02 07:53:24 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,1 +1,13 @@
+import { Link } from 'react-router-dom'
 
+export default function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4 flex gap-4">
+      <Link to="/" className="hover:underline">Home</Link>
+      <Link to="/ativos" className="hover:underline">Ativos</Link>
+      <Link to="/mercado" className="hover:underline">Mercado</Link>
+      <Link to="/recomendacoes" className="hover:underline">Recomendações</Link>
+      <Link to="/educacao" className="hover:underline">Educação</Link>
+    </nav>
+  )
+}

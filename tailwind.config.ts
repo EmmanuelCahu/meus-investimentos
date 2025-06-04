2b22d0e20651e19264bac9cd94d6810b683f29d7
EmmanuelCahu <134721876+EmmanuelCahu@users.noreply.github.com>
2025-06-04 16:51:19 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,25 @@
-// tailwind.config.js
+import type { Config } from 'tailwindcss'
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  // Permite alternar dark mode via a classe "dark" no <html> ou <body>
-  darkMode: 'class',
-
+const config: Config = {
+  darkMode: 'class', // ativa dark mode via classe "dark" no <html> ou <body>
   content: [
-    // Ajuste essas paths conforme sua estrutura. 
     './src/**/*.{js,jsx,ts,tsx}',
     './public/index.html',
   ],
-
   theme: {
     extend: {
-      // Definindo keyframes para fade-in
       keyframes: {
         'fade-in': {
           '0%': { opacity: '0' },
           '100%': { opacity: '1' },
         },
       },
-      // Criando uma animação utilitária chamada "fade-in"
       animation: {
         'fade-in': 'fade-in 0.3s ease-out forwards',
       },
     },
   },
-
   plugins: [],
 }
+
+export default config

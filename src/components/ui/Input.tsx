5de3a2d85efe571df23a4204bc21f141c073b7a2
EmmanuelCahu <134721876+EmmanuelCahu@users.noreply.github.com>
2025-06-04 17:18:31 +0200

    Create Input.tsx

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,17 @@
+import { InputHTMLAttributes, forwardRef } from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => {
+  return (
+    <input
+      ref={ref}
+      className={`w-full px-4 py-2 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
+      {...props}
+    />
+  );
+});
+
+Input.displayName = "Input";
+
+export default Input;

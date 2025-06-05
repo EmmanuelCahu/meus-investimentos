88f37f63b833588f6ce9a1237b464ba080d3e878
EmmanuelCahu <134721876+EmmanuelCahu@users.noreply.github.com>
2025-06-05 14:10:18 +0200

    Update useAtivosData.ts

--- src/hooks/useAtivosData.ts
+++ src/hooks/useAtivosData.ts
@@ -1,1 +1,31 @@
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@lib/firebase"; // aponta para src/firebase/config.ts
+import { Ativo } from "@/types/Ativo";
 
+export function useAtivosData() {
+  const [ativos, setAtivos] = useState<Ativo[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchAtivos = async () => {
+      try {
+        const ativosCollection = collection(db, "ativos");
+        const ativosSnapshot = await getDocs(ativosCollection);
+        const ativosList = ativosSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        })) as Ativo[];
+        setAtivos(ativosList);
+      } catch (error) {
+        console.error("Erro ao buscar ativos:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAtivos();
+  }, []);
+
+  return { ativos, loading };
+}

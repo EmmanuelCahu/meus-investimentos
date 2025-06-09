690e9bee10f5ba9bcb94598c8c906938c62d3dd7
EmmanuelCahu <134721876+EmmanuelCahu@users.noreply.github.com>
2025-06-09 14:32:32 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import HomePage from './pages/HomePage';
+import { AuthPage } from './pages/AuthPage';
 
-function App() {
-  return <HomePage />;
+export default function App() {
+  return <AuthPage />;
 }
-
-export default App;

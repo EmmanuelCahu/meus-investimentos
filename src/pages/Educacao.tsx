4f01aaf69c86673e12295cd310852656e39310d7
EmmanuelCahu <134721876+EmmanuelCahu@users.noreply.github.com>
2025-06-09 14:55:59 -0300

    Create Educacao.tsx

---
+++ src/pages/Educacao.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Educacao() {
+  return (
+    <div className="p-6">
+      <h2 className="text-2xl font-semibold mb-4">Educação</h2>
+      <p className="text-gray-700">Aprenda mais sobre investimentos, renda passiva e ativos.</p>
+    </div>
+  );
+}
